3d0b7f5685a3736dbe5c71631641d98c99525d6a
CoolBreezeHu <67082422+CoolBreezeHu@users.noreply.github.com>
2022-12-20 14:45:15 +0800

    V0.2 反转标志位
    用于测试调试功能（逻辑分析仪等）

--- Source/main.c
+++ Source/main.c
@@ -1,5 +1,24 @@
+/**************************************************************************//**
+ * @file     main.c
+ * @brief    the main function file for project
+ *
+ * @author   huxing 
+ * @date     2022-12-20 09:30:44
+ ******************************************************************************/
+int g_flag = 0;
+
+void delay(int n)
+{
+	while(n--);
+}	
 
 int main()
 {
-	while(1);
+	for (;;)
+	{
+		delay(10000);
+		g_flag = 1;
+		delay(10000);
+		g_flag = 0;
+	}
 }
